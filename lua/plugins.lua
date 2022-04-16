a7bd26f7b1582e7d0d42898dbfdc831df0431805
menory <hhw_menory@yeah.net>
2022-04-17 01:04:27 +0800

    feat: 自动完成 & 代码提示

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -388,6 +388,149 @@ return require("packer").startup(function(use)
 		end,
 	})
 
+	-- 自动完成
+	-- Copilot setup
+	use({
+		"github/copilot.vim",
+	})
+
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
+	-- use "hrsh7th/cmp-omni"
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	-- For luasnip user.
+	use({
+		"L3MON4D3/LuaSnip",
+		requires = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	})
+	use("saadparwaiz1/cmp_luasnip")
+	use({
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			-- local luasnip = require("luasnip")
+			local cmp = require("cmp")
+			cmp.setup({
+				window = {
+					documentation = {
+						border = "single",
+					},
+				},
+				snippet = {
+					expand = function(args)
+						-- For `vsnip` user.
+						-- vim.fn["vsnip#anonymous"](args.body)
+
+						-- For `luasnip` user.
+						require("luasnip").lsp_expand(args.body)
+
+						-- For `ultisnips` user.
+						-- vim.fn["UltiSnips#Anon"](args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- item go down and up
+					["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+					["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+					-- doc scrool
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
+					["<C-u>"] = cmp.mapping.scroll_docs(4),
+					-- confirm
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					-- https://github.com/hrsh7th/nvim-cmp#what-is-the-pairs-wise-plugin-automatically-supported
+					["<CR>"] = cmp.mapping.confirm({
+						-- behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					-- 	["<Tab>"] = cmp.mapping(function(fallback)
+					-- 		if cmp.visible() then
+					-- 			cmp.select_next_item()
+					-- 		elseif luasnip.expand_or_jumpable() then
+					-- 			luasnip.expand_or_jump()
+					-- 		elseif has_words_before() then
+					-- 			cmp.complete()
+					-- 		else
+					-- 			fallback()
+					-- 		end
+					-- 	end, {
+					-- 		"i",
+					-- 		"s",
+					-- 	}),
+					-- 	["<S-Tab>"] = cmp.mapping(function(fallback)
+					-- 		if cmp.visible() then
+					-- 			cmp.select_prev_item()
+					-- 		elseif luasnip.jumpable(-1) then
+					-- 			luasnip.jump(-1)
+					-- 		else
+					-- 			fallback()
+					-- 		end
+					-- 	end, {
+					-- 		"i",
+					-- 		"s",
+					-- 	}),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					-- For vsnip user.
+					-- { name = "vsnip" },
+					-- For luasnip user.
+					{ name = "luasnip" },
+					-- For ultisnips user.
+					-- { name = "ultisnips" },
+					{ name = "crates" },
+					{ name = "cmp_tabnine" },
+					{ name = "nvim_lua" },
+					-- { name = "omni" },
+					{ name = "buffer" },
+				},
+				-- formatting = {
+				-- 	format = require("lsp").cmp_format({
+				-- 		with_text = true,
+				-- 		menu = {
+				-- 			buffer = "[Buffer]",
+				-- 			nvim_lsp = "[LSP]",
+				-- 			nvim_lua = "[Lua]",
+				-- 			luasnip = "[LuaSnip]",
+				-- 			ultisnips = "[Ultisnips]",
+				-- 			crates = "[Crates]",
+				-- 			cmp_tabnine = "[TabNine]",
+				-- 			latex_symbols = "[Latex]",
+				-- 		},
+				-- 	}),
+				-- },
+			})
+		end,
+	})
+	use({
+		"tzachar/cmp-tabnine",
+		run = "./install.sh",
+		requires = "hrsh7th/nvim-cmp",
+		-- https://github.com/tzachar/cmp-tabnine#setup
+		config = function()
+			local tabnine = require("cmp_tabnine.config")
+			tabnine:setup({
+				max_lines = 1000,
+				max_num_results = 20,
+				sort = true,
+				run_on_every_keystroke = true,
+				snippet_placeholder = "..",
+			})
+		end,
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
