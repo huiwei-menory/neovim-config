d4fe3182d606a09c58564f3005fb97d68bd7ce15
menory <hhw_menory@yeah.net>
2022-04-16 20:28:20 +0800

    feat: 格式化

--- init.lua
+++ init.lua
@@ -6,16 +6,25 @@
 local packer_bootstrap = nil
 
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  print("packer_bootstrap...")
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("packer_bootstrap done")
+	print("packer_bootstrap...")
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("packer_bootstrap done")
 end
 
-vim.cmd [[autocmd BufWritePost general.lua source <afile> | PackerCompile]]
-vim.cmd [[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]]
+vim.g.mapleader = [[ ]]
+
+vim.cmd([[autocmd BufWritePost general.lua source <afile> | PackerCompile]])
+vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 
 require("general")
 require("plugins")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,73 +1,113 @@
 return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use "wbthomason/packer.nvim"
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- 文件浏览器
-  -- https://github.com/kyazdani42/nvim-tree.lua
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
-    config = function()
-      vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", {})
-      require("nvim-tree").setup({
-        -- open the tree when running this setup function
-        open_on_setup = true,
-        open_on_setup_file = true,
-        open_on_tab = true,
-        -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-        update_focused_file = {
-          -- enables the feature
-          enable = true,
-          -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-          -- only relevant when `update_focused_file.enable` is true
-          update_cwd = false,
-          -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-          -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-          ignore_list = {},
-        },
-      })
-    end
-  }
+	-- 文件浏览器
+	-- https://github.com/kyazdani42/nvim-tree.lua
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+		config = function()
+			vim.api.nvim_set_keymap("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", {})
+			require("nvim-tree").setup({
+				-- open the tree when running this setup function
+				open_on_setup = true,
+				open_on_setup_file = true,
+				open_on_tab = true,
+				-- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+				update_focused_file = {
+					-- enables the feature
+					enable = true,
+					-- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+					-- only relevant when `update_focused_file.enable` is true
+					update_cwd = false,
+					-- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+					-- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+					ignore_list = {},
+				},
+			})
+		end,
+	})
 
-  -- 配色方案
-  use {
-    "EdenEast/nightfox.nvim",
-    config = function()
-      vim.cmd("colorscheme nightfox")
-    end,
-  }
+	-- 配色方案
+	use({
+		"EdenEast/nightfox.nvim",
+		config = function()
+			vim.cmd("colorscheme nightfox")
+		end,
+	})
 
-  -- 启动页
-  -- https://github.com/mhinz/vim-startify
-  use("mhinz/vim-startify")
+	-- 启动页
+	-- https://github.com/mhinz/vim-startify
+	use("mhinz/vim-startify")
 
-  -- 状态行    
-  -- https://github.com/itchyny/lightline.vim
-  use({    
-    "itchyny/lightline.vim",    
-    config = function()    
-      vim.g.lightline = {    
-        ["enable"] = { ["statusline"] = 1, ["tabline"] = 1 },    
-        -- ["colorscheme"] = "nightfox",    
-        ["active"] = {    
-          ["left"] = { { "mode", "paste" }, { "gitbranch", "readonly", "filename", "modified" } },    
-        },    
-        ["tabline"] = {    
-          ["left"] = { { "tabs" } },                                                                                                                                           
-          ["right"] = { { "close" } },    
-        },    
-        ["component_function"] = {    
-          ["gitbranch"] = "FugitiveHead",    
-        },    
-      }    
-    end,    
-  })
+	-- 状态行
+	-- https://github.com/itchyny/lightline.vim
+	use({
+		"itchyny/lightline.vim",
+		config = function()
+			vim.g.lightline = {
+				["enable"] = { ["statusline"] = 1, ["tabline"] = 1 },
+				-- ["colorscheme"] = "nightfox",
+				["active"] = {
+					["left"] = { { "mode", "paste" }, { "gitbranch", "readonly", "filename", "modified" } },
+				},
+				["tabline"] = {
+					["left"] = { { "tabs" } },
+					["right"] = { { "close" } },
+				},
+				["component_function"] = {
+					["gitbranch"] = "FugitiveHead",
+				},
+			}
+		end,
+	})
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	-- 格式化
+	-- https://github.com/mhartington/formatter.nvim
+	use({
+		"mhartington/formatter.nvim",
+		config = function()
+			require("formatter").setup({
+				logging = true,
+				-- https://github.com/mhartington/formatter.nvim/blob/master/CONFIG.md
+				filetype = {
+					lua = {
+						function()
+							return {
+								-- paru stylua for archlinux
+								exe = "stylua",
+								args = {
+									"--search-parent-directories",
+									"--stdin-filepath",
+									vim.api.nvim_buf_get_name(0),
+									"-",
+								},
+								stdin = true,
+							}
+						end,
+					},
+					javascript = {
+						-- prettier
+						function()
+							return {
+								exe = "prettier",
+								args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote" },
+								stdin = true,
+							}
+						end,
+					},
+				},
+			})
+			vim.api.nvim_set_keymap("n", "<leader>kf", "<cmd>Format<cr>", { noremap = true, silent = true })
+		end,
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
